fc871bbb1d15f503051cc4db35e8047848a62880
omartinez <omartinez@ifae.es>
2021-03-19 16:34:38 +0100

    Edited setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 __author__ = 'Oscar Martínez'
 __copyright__ = 'Copyleft 2021'
-__date__ = '25/2/21'
+__date__ = '19/03/21'
 __credits__ = ['Oscar Martínez', ]
 __license__ = 'CC0 1.0 Universal'
 __version__ = '0.1'
@@ -11,7 +11,9 @@ __maintainer__ = 'Oscar Martínez'
 __email__ = 'omartinez@ifae.es'
 
 import os
-from setuptools import setup, find_packages
+
+import setuptools
+from setuptools import find_packages
 
 # Read README and CHANGES files for the long description
 here = os.path.abspath(os.path.dirname(__file__))
@@ -19,8 +21,8 @@ with open(os.path.join(here, 'README.md')) as fh:
       long_description = fh.read()
 
 # print(find_packages(exclude=('tests',)))
-setup(
-      name="pirats-lib",
+setuptools.setup(
+      name="pyvsr53dl",
       version="0.1.0",
       description="Python Library to interface with Thyracont's VSR53DL Vacuum Sensor through RS458 interface",
       long_description_content_type="text/markdown",
